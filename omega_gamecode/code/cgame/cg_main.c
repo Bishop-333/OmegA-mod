6df672d5f557d7d939f9265f9b9d430502511a81
Bishop-333 <99912467+Bishop-333@users.noreply.github.com>
2024-03-20 12:58:15 +0100

    default fov 115->110

--- omega_gamecode/code/cgame/cg_main.c
+++ omega_gamecode/code/cgame/cg_main.c
@@ -325,7 +325,7 @@ static cvarTable_t cvarTable[] = { // bk001129
 	{ &cg_autoswitch, "cg_autoswitch", "0", CVAR_ARCHIVE },
 	{ &cg_drawGun, "cg_drawGun", "1", CVAR_ARCHIVE },
 	{ &cg_zoomFov, "cg_zoomfov", "22.5", CVAR_ARCHIVE },
-	{ &cg_fov, "cg_fov", "115", CVAR_ARCHIVE },
+	{ &cg_fov, "cg_fov", "110", CVAR_ARCHIVE },
 	{ &cg_viewsize, "cg_viewsize", "100", CVAR_ARCHIVE },
 	{ &cg_shadows, "cg_shadows", "1", CVAR_ARCHIVE  },
 	{ &cg_gibs, "cg_gibs", "1", CVAR_ARCHIVE  },
