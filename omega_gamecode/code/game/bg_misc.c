6c904cd40cca133b94214229c5557961e78bc266
Bishop-333 <99912467+Bishop-333@users.noreply.github.com>
2026-07-24 23:50:05 +0200

    Update bg_misc.c

--- omega_gamecode/code/game/bg_misc.c
+++ omega_gamecode/code/game/bg_misc.c
@@ -1566,7 +1566,6 @@ and after local prediction on the client
 ========================
 */
 void BG_PlayerStateToEntityState( playerState_t *ps, entityState_t *s, qboolean snap ) {
-	int i;
 
 	if ( ps->pm_type == PM_INTERMISSION || ps->pm_type == PM_SPECTATOR ) {
 		s->eType = ET_INVISIBLE;
@@ -1586,51 +1585,7 @@ void BG_PlayerStateToEntityState( playerState_t *ps, entityState_t *s, qboolean 
 	// set the trDelta for flag direction
 	VectorCopy( ps->velocity, s->pos.trDelta );
 
-	s->apos.trType = TR_INTERPOLATE;
-	VectorCopy( ps->viewangles, s->apos.trBase );
-	if ( snap ) {
-		SnapVector( s->apos.trBase );
-	}
-
-	s->angles2[YAW] = ps->movementDir;
-	s->legsAnim = ps->legsAnim;
-	s->torsoAnim = ps->torsoAnim;
-	s->clientNum = ps->clientNum; // ET_PLAYER looks here instead of at number
-	                              // so corpses can also reference the proper config
-	s->eFlags = ps->eFlags;
-	if ( ps->stats[STAT_HEALTH] <= 0 ) {
-		s->eFlags |= EF_DEAD;
-	} else {
-		s->eFlags &= ~EF_DEAD;
-	}
-
-	if ( ps->externalEvent ) {
-		s->event = ps->externalEvent;
-		s->eventParm = ps->externalEventParm;
-	} else if ( ps->entityEventSequence < ps->eventSequence ) {
-		int seq;
-
-		if ( ps->entityEventSequence < ps->eventSequence - MAX_PS_EVENTS ) {
-			ps->entityEventSequence = ps->eventSequence - MAX_PS_EVENTS;
-		}
-		seq = ps->entityEventSequence & ( MAX_PS_EVENTS - 1 );
-		s->event = ps->events[seq] | ( ( ps->entityEventSequence & 3 ) << 8 );
-		s->eventParm = ps->eventParms[seq];
-		ps->entityEventSequence++;
-	}
-
-	s->weapon = ps->weapon;
-	s->groundEntityNum = ps->groundEntityNum;
-
-	s->powerups = 0;
-	for ( i = 0; i < MAX_POWERUPS; i++ ) {
-		if ( ps->powerups[i] ) {
-			s->powerups |= 1 << i;
-		}
-	}
-
-	s->loopSound = ps->loopSound;
-	s->generic1 = ps->generic1;
+	BG_PlayerStateToEntityState_Common( ps, s, snap );
 }
 
 /*
@@ -1642,7 +1597,6 @@ and after local prediction on the client
 ========================
 */
 void BG_PlayerStateToEntityStateExtraPolate( playerState_t *ps, entityState_t *s, int time, qboolean snap ) {
-	int i;
 
 	if ( ps->pm_type == PM_INTERMISSION || ps->pm_type == PM_SPECTATOR ) {
 		s->eType = ET_INVISIBLE;
@@ -1666,51 +1620,7 @@ void BG_PlayerStateToEntityStateExtraPolate( playerState_t *ps, entityState_t *s
 	// set maximum extra polation time
 	s->pos.trDuration = 25; // 1000 / sv_fps (default = 20)
 
-	s->apos.trType = TR_INTERPOLATE;
-	VectorCopy( ps->viewangles, s->apos.trBase );
-	if ( snap ) {
-		SnapVector( s->apos.trBase );
-	}
-
-	s->angles2[YAW] = ps->movementDir;
-	s->legsAnim = ps->legsAnim;
-	s->torsoAnim = ps->torsoAnim;
-	s->clientNum = ps->clientNum; // ET_PLAYER looks here instead of at number
-	                              // so corpses can also reference the proper config
-	s->eFlags = ps->eFlags;
-	if ( ps->stats[STAT_HEALTH] <= 0 ) {
-		s->eFlags |= EF_DEAD;
-	} else {
-		s->eFlags &= ~EF_DEAD;
-	}
-
-	if ( ps->externalEvent ) {
-		s->event = ps->externalEvent;
-		s->eventParm = ps->externalEventParm;
-	} else if ( ps->entityEventSequence < ps->eventSequence ) {
-		int seq;
-
-		if ( ps->entityEventSequence < ps->eventSequence - MAX_PS_EVENTS ) {
-			ps->entityEventSequence = ps->eventSequence - MAX_PS_EVENTS;
-		}
-		seq = ps->entityEventSequence & ( MAX_PS_EVENTS - 1 );
-		s->event = ps->events[seq] | ( ( ps->entityEventSequence & 3 ) << 8 );
-		s->eventParm = ps->eventParms[seq];
-		ps->entityEventSequence++;
-	}
-
-	s->weapon = ps->weapon;
-	s->groundEntityNum = ps->groundEntityNum;
-
-	s->powerups = 0;
-	for ( i = 0; i < MAX_POWERUPS; i++ ) {
-		if ( ps->powerups[i] ) {
-			s->powerups |= 1 << i;
-		}
-	}
-
-	s->loopSound = ps->loopSound;
-	s->generic1 = ps->generic1;
+	BG_PlayerStateToEntityState_Common( ps, s, snap );
 }
 
 /*
